012540ab1ab8e5ce8b79b1ae40c3a4231efa7d0f
Aniruddh Vishwakarma <vishwaaniruddh@gmail.com>
2026-07-02 21:45:15 +0530

    added gemini chat

---
+++ Config/secrets.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'GROQ_API_KEY' => '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',
+    'GEMINI_API_KEY' => 'AQ.Ab8RN6LbpLTcmpeyCAlDMe3qQvN77N1iCWNGWi9yCpzn066y9A',
+];
