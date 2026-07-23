629e59496809fddea8b575ced7b0de60faa3a249
Aniruddh Vishwakarma <vishwaaniruddh@gmail.com>
2026-07-23 18:45:47 +0530

    added database

--- Core/Database.php
+++ Core/Database.php
@@ -9,14 +9,25 @@ class Database {
             unset(self::$instances[$type]); // Connection died, clear it so it reconnects
         }
 
-        if (!isset(self::$instances[$type])) {
-            // Using include instead of require_once to ensure variables are available in this scope
-            // even if the file was previously included elsewhere.
-            // Include legacy config
-            include(__DIR__ . '/../../API/config.php');
+        if (!isset(self::$instances[$type]) || !self::$instances[$type]) {
+            global $con, $con3;
+            require_once(__DIR__ . '/../../API/config.php');
             
-            if (isset($con)) self::$instances['con'] = $con;
-            if (isset($con3)) self::$instances['con3'] = $con3;
+            if ($type === 'con' || $type === 'conn') {
+                if (isset($GLOBALS['con']) && $GLOBALS['con']) self::$instances['con'] = $GLOBALS['con'];
+                else if (isset($con) && $con) self::$instances['con'] = $con;
+            } else if ($type === 'con3') {
+                if (isset($GLOBALS['con3']) && $GLOBALS['con3']) {
+                    self::$instances['con3'] = $GLOBALS['con3'];
+                } else if (isset($con3) && $con3) {
+                    self::$instances['con3'] = $con3;
+                } else {
+                    $is_local = isset($_SERVER['HTTP_HOST']) && (in_array($_SERVER['HTTP_HOST'], ['localhost', '127.0.0.1', '::1']) || strpos($_SERVER['HTTP_HOST'], 'localhost:') === 0);
+                    $con3_conn = $is_local ? @mysqli_connect("localhost", "root", "", "u464193275_srishringarr") : @mysqli_connect("localhost", "u464193275_sarmicropos", "Mypos1234", "u464193275_srishringarr");
+                    $GLOBALS['con3'] = $con3_conn;
+                    self::$instances['con3'] = $con3_conn;
+                }
+            }
 
             // Handle lazy loading for remote 'woo' connection
             if ($type === 'woo') {
